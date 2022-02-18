84a4678816f068fc4cef295b4a49ec3f217f1b7e
94mark <90877724+94mark@users.noreply.github.com>
2022-02-18 22:07:41 +0900

    SFX Audio set

--- BattleTank/Assets/Scripts/FireCannon.cs
+++ BattleTank/Assets/Scripts/FireCannon.cs
@@ -6,10 +6,15 @@ public class FireCannon : MonoBehaviour
 {
     public GameObject cannon = null;
     public Transform firePos;
+    private AudioClip fireSfx = null;
+    private AudioSource sfx = null;
+
     // Start is called before the first frame update
     void Awake()
     {
         cannon = (GameObject)Resources.Load("Cannon");
+        fireSfx = Resources.Load<AudioClip>("CannonFire");
+        sfx = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +27,7 @@ public class FireCannon : MonoBehaviour
     }
     void Fire()
     {
+        sfx.PlayOneShot(fireSfx, 1.0f);
         Instantiate(cannon, firePos.position, firePos.rotation);
     }
 }
