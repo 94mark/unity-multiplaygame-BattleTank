14da644cd16401a6a02073970f7f6e5645e5efd7
94mark <90877724+94mark@users.noreply.github.com>
2022-02-19 16:53:03 +0900

    Update TurretControl.cs
    터렛 동기화

--- BattleTank/Assets/Scripts/TurretControl.cs
+++ BattleTank/Assets/Scripts/TurretControl.cs
@@ -1,30 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class TurretControl : MonoBehaviour
+public class TurretControl : MonoBehaviourPun, IPunObservable
 {
     private Transform tr;
     private RaycastHit hit;
     public float rotSpeed = 5.0f;
 
+    private PhotonView pv = null;
+    private Quaternion currRot = Quaternion.identity;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         tr = GetComponent<Transform>();
+
+        pv = GetComponent<PhotonView>();
+        pv.ObservedComponents[0] = this; //PhotonView의 Observed 속성을 이 스크립트로 지정
+        pv.Synchronization = ViewSynchronization.UnreliableOnChange; //Phton View 동기화 속성 설정
+        currRot = tr.localRotation; //초기 회전 값 설정
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
-
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<8))
+        if (pv.IsMine) //자신의 탱크일 때만 조정
         {
-            Vector3 relative = tr.InverseTransformPoint(hit.point);
-            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
-            tr.Rotate(0, angle * Time.deltaTime * rotSpeed, 0);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 8))
+            {
+                Vector3 relative = tr.InverseTransformPoint(hit.point);
+                float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+                tr.Rotate(0, angle * Time.deltaTime * rotSpeed, 0);
+            }
+        }
+        else //원격 탱크일 경우
+        {
+            tr.localRotation = Quaternion.Slerp(tr.localRotation, currRot, Time.deltaTime * 3.0f);
+        }        
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(tr.localRotation);
+        }
+        else
+        {
+            currRot = (Quaternion)stream.ReceiveNext();
         }
     }
 }
