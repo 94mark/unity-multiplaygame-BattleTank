5d1847dc933c103ea7a96355d58cfc4fbea76e1d
94mark <90877724+94mark@users.noreply.github.com>
2022-02-19 15:20:29 +0900

    Update PhotonInit.cs

--- BattleTank/Assets/Scripts/PhotonInit.cs
+++ BattleTank/Assets/Scripts/PhotonInit.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class PhotonInit : MonoBehaviourPunCallbacks
 {
@@ -15,6 +16,25 @@ public class PhotonInit : MonoBehaviourPunCallbacks
     private void OnGUI()
     {
         GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
+    }
+    public override void OnConnectedToMaster() // 포톤 클라우드에 접속이 잘 되면 호출되는 콜백함수
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Entered Lobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log("No Room!");
+        PhotonNetwork.CreateRoom("MyRoom", new RoomOptions { MaxPlayers = 20 }); //최대 20명 방 만들어줌
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Enter Room");
     }
 
     // Update is called once per frame
