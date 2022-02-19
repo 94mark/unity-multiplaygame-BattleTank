66c3177d8aabef9fb94c438fe3bf01868cafb5cc
94mark <90877724+94mark@users.noreply.github.com>
2022-02-19 17:11:36 +0900

    Update FireCannon.cs
    포탄 발사 동기화

--- BattleTank/Assets/Scripts/FireCannon.cs
+++ BattleTank/Assets/Scripts/FireCannon.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class FireCannon : MonoBehaviour
+public class FireCannon : MonoBehaviourPun
 {
     public GameObject cannon = null;
     public Transform firePos;
     private AudioClip fireSfx = null;
     private AudioSource sfx = null;
+
+    private PhotonView pv = null;
+
 
     // Start is called before the first frame update
     void Awake()
@@ -15,16 +19,20 @@ public class FireCannon : MonoBehaviour
         cannon = (GameObject)Resources.Load("Cannon");
         fireSfx = Resources.Load<AudioClip>("CannonFire");
         sfx = GetComponent<AudioSource>();
+        pv = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+    {        
+        if(pv.IsMine && Input.GetMouseButtonDown(0))
         {
             Fire();
+            //원격 네트워크 플레이어의 탱크에 RPC 원격으로 Fire 함수 호출
+            pv.RPC("Fire", RpcTarget.Others, null);
         }
     }
+    [PunRPC]
     void Fire()
     {
         sfx.PlayOneShot(fireSfx, 1.0f);
