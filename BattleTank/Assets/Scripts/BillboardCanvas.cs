341bc7c30915641e67c8074018014ba5be735eff
94mark <90877724+94mark@users.noreply.github.com>
2022-02-20 16:12:24 +0900

    Update BillboardCanvas.cs

--- BattleTank/Assets/Scripts/BillboardCanvas.cs
+++ BattleTank/Assets/Scripts/BillboardCanvas.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BillboardCanvas : MonoBehaviour
 {
+    private Transform tr;
+    private Transform mainCameraTr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tr = GetComponent<Transform>();
+        mainCameraTr = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tr.LookAt(mainCameraTr);
     }
 }
